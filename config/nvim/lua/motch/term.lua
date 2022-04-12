2224888932cb1b9fe08317114383156a2c886cca
Mitchell Hanberg <mitch@mitchellhanberg.com>
2022-04-11 23:30:02 -0400

    generic term module

---
+++ config/nvim/lua/motch/term.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+local nil_buf_id = 999999
+local term_buf_id = nil_buf_id
+
+function M.open(cmd, winnr, notifier)
+  -- delete the current buffer if it's still open
+  if vim.api.nvim_buf_is_valid(term_buf_id) then
+    vim.api.nvim_buf_delete(term_buf_id, { force = true })
+    term_buf_id = nil_buf_id
+  end
+
+  vim.cmd("botright new | lua vim.api.nvim_win_set_height(0, 15)")
+  term_buf_id = vim.api.nvim_get_current_buf()
+  vim.opt_local.number = false
+  vim.opt_local.cursorline = false
+
+  vim.fn.termopen(cmd, {
+    on_exit = function(_jobid, exit_code, _event)
+      if notifier then
+        notifier(cmd, exit_code)
+      end
+
+      if exit_code == 0 then
+        vim.api.nvim_buf_delete(term_buf_id, { force = true })
+        term_buf_id = nil_buf_id
+      end
+    end,
+  })
+
+  print(cmd)
+
+  vim.cmd([[normal! G]])
+  vim.cmd(winnr .. [[wincmd w]])
+end
+
+return M
--- config/nvim/plugin/test.lua
+++ config/nvim/plugin/test.lua
@@ -4,9 +4,6 @@ vim.keymap.set("n", "<leader>f", ":TestFile<cr>")
 vim.keymap.set("n", "<leader>s", ":TestSuite<cr>")
 vim.keymap.set("n", "<leader>l", ":TestLast<cr>")
 
-local nil_buf_id = 999999
-local term_buf_id = nil_buf_id
-
 local vim_notify_notfier = function(exit)
   if exit == 0 then
     vim.notify("Success: " .. cmd, vim.log.levels.INFO)
@@ -24,38 +21,12 @@ local terminal_notifier_notfier = function(cmd, exit)
   end
 end
 
-vim.g.better_vim_test_runner_notifier = terminal_notifier_notfier
-
-local function test(cmd)
-  -- delete the current buffer if it's still open
-  if vim.api.nvim_buf_is_valid(term_buf_id) then
-    vim.api.nvim_buf_delete(term_buf_id, { force = true })
-    term_buf_id = nil_buf_id
-  end
-
-  vim.cmd("botright new | lua vim.api.nvim_win_set_height(0, 15)")
-  term_buf_id = vim.api.nvim_get_current_buf()
-  vim.opt_local.number = false
-  vim.opt_local.cursorline = false
-
-  vim.fn.termopen(cmd, {
-    on_exit = function(_jobid, exit_code, _event)
-      vim.g.better_vim_test_runner_notifier(cmd, exit_code)
-
-      if exit_code == 0 then
-        vim.api.nvim_buf_delete(term_buf_id, { force = true })
-        term_buf_id = nil_buf_id
-      end
-    end,
-  })
-
-  print(cmd)
-
-  vim.cmd([[normal! G]])
-  vim.cmd([[wincmd p]])
-end
-
 -- vim.g["test#javascript#jest#executable"] = "bin/test"
 
-vim.g["test#custom_strategies"] = { motch = test }
+vim.g["test#custom_strategies"] = {
+  motch = function(cmd)
+    local winnr = vim.fn.winnr()
+    require("motch.term").open(cmd, winnr, terminal_notifier_notfier)
+  end,
+}
 vim.g["test#strategy"] = "motch"
