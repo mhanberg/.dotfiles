f08b06a21fdc5928847b4d79c44feb09b34c2219
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-02-13 11:04:49 -0500

    disable silicon theme generation

--- config/nvim/plugin/colorscheme.lua
+++ config/nvim/plugin/colorscheme.lua
@@ -1,13 +1,13 @@
-local silicon_utils = require("silicon.utils")
-vim.api.nvim_create_augroup("SiliconRefresh", { clear = true })
-vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-  group = "SiliconRefresh",
-  callback = function()
-    silicon_utils.build_tmTheme()
-    silicon_utils.reload_silicon_cache { async = true }
-  end,
-  desc = "Reload silicon themes cache on colorscheme switch",
-})
+-- local silicon_utils = require("silicon.utils")
+-- vim.api.nvim_create_augroup("SiliconRefresh", { clear = true })
+-- vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+--   group = "SiliconRefresh",
+--   callback = function()
+--     silicon_utils.build_tmTheme()
+--     silicon_utils.reload_silicon_cache { async = true }
+--   end,
+--   desc = "Reload silicon themes cache on colorscheme switch",
+-- })
 
 vim.opt.termguicolors = true
 
