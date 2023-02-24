8ac7a664a91090ba15d17bc419857eb1633ce4c7
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-02-23 21:08:43 -0500

    add some noice config changes

--- config/nvim/lua/motch/lazy.lua
+++ config/nvim/lua/motch/lazy.lua
@@ -217,6 +217,11 @@ require("lazy").setup({
         backend = "mini",
       },
       lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
         message = {
           view = "mini",
         },
