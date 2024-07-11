788b6e405171a14cff2e09925dcef319ecefd3d5
Mitchell Hanberg <mitch@mitchellhanberg.com>
2024-07-11 15:17:45 -0400

    work: use simple ghostty

--- nix/home/work.nix
+++ nix/home/work.nix
@@ -1,6 +1,6 @@
 {...}: {
   imports = [./common.nix];
 
-  programs.ghostty.settings.config-file = "/Users/mitchell/.config/ghostty/themes/KanagwaWave";
+  programs.ghostty.settings.config-file = "/Users/mitchell/.config/ghostty/themes/SimpleGhostty";
   programs.git.extraConfig.gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
 }
