442d21e3cab9d62a1b32a16524a61b990f85e0c3
Mitchell Hanberg <mitch@mitchellhanberg.com>
2025-01-15 13:46:06 -0500

    personal: remove signers file

--- nix/home/personal.nix
+++ nix/home/personal.nix
@@ -15,6 +15,5 @@ in {
   '';
   programs.git.extraConfig.gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
   programs.git.extraConfig.gpg.format = "ssh";
-  programs.git.extraConfig.gpg.ssh.allowedSignersFile = "/Users/mitchell/.ssh/allowed_signers";
   programs.git.extraConfig.commit.gpgSign = true;
 }
