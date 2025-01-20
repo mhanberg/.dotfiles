38fc09b7eaaa7641a0431055c3d3a13011ea2037
Mitchell Hanberg <mitch@mitchellhanberg.com>
2025-01-20 10:33:31 -0500

    fix: nixos config

--- nix/home/nixos.nix
+++ nix/home/nixos.nix
@@ -1,8 +1,6 @@
-{
-  lib,
-  pkgs,
-  ...
-}: {
+{ lib, pkgs, ... }: let
+  common = pkgs.callPackage ./packages.nix {inherit pkgs;};
+in {
   home.username = "mitchell";
   home.homeDirectory = "/home/mitchell";
   imports = [
@@ -10,8 +8,8 @@
     ./themes/rose-pine.nix
   ];
 
+  home.packages = common.packages;
   programs.git.extraConfig.gpg.ssh.program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
   programs.git.extraConfig.gpg.format = "ssh";
-  programs.git.extraConfig.gpg.ssh.allowedSignersFile = "/Users/mitchell/.ssh/allowed_signers";
   programs.git.extraConfig.commit.gpgSign = true;
 }
