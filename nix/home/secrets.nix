13a791e9d9205a4782e6ca453ee0c24fcac1f2f0
Mitchell Hanberg <mitch@mitchellhanberg.com>
2025-04-30 15:43:16 -0400

    add unused nublar public key

--- nix/home/secrets.nix
+++ nix/home/secrets.nix
@@ -1,5 +1,6 @@
 let
   dk = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBawt44pYaVCd7wxRtsxJfcvMP4ac6Ara9xd7YK8mvq";
+  # nublar = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOFqwZQR1Z5Y9Tss3HvBN/bEPx5FthMjTHSuZhjmh+lu";
 in {
   "export-aws-keys.age".publicKeys = [dk];
 }
