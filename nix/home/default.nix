e91370f4fa092e10ca2f14a63821489b3da8cd7b
Mitchell Hanberg <mitch@mitchellhanberg.com>
2025-05-26 20:07:36 -0400

    fix: update lazygit config

--- nix/home/default.nix
+++ nix/home/default.nix
@@ -454,7 +454,7 @@ in {
           verbose = "default";
         };
         branchLogCmd = "git log --graph --color=always --abbrev-commit --decorate --date=relative --pretty=medium {{branchName}} --";
-        allBranchesLogCmd = "git log --graph --all --color=always --abbrev-commit --decorate --date=relative  --pretty=medium";
+        allBranchesLogCmds = ["git log --graph --all --color=always --abbrev-commit --decorate --date=relative  --pretty=medium"];
         overrideGpg = false;
         disableForcePushing = false;
         confirmOnQuit = false;
