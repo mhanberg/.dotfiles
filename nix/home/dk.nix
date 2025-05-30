caa7a02d3be908812c46c2b140814cf3153db9d6
Mitchell Hanberg <mitch@mitchellhanberg.com>
2025-05-30 10:38:29 -0400

    fix: script

--- nix/home/dk.nix
+++ nix/home/dk.nix
@@ -4,6 +4,10 @@
   ...
 } @ args: let
   myLib = import ../lib.nix args;
+  export-aws-vars = import ./dk/export-aws-vars.nix {
+    inherit pkgs;
+    age-secrets = config.age;
+  };
 in {
   imports = [
     ./themes/rose-pine-moon.nix
@@ -13,43 +17,8 @@ in {
   home.homeDirectory = "/Users/m.hanberg";
   age.secrets.export-aws-keys.file = ./export-aws-keys.age;
 
-  home.packages = with pkgs; [
-    (writeShellApplication {
-      name = "export-aws-vars";
-      runtimeInputs = [awscli2];
-      text = ''
-        main() {
-          cmd="AWS_PROFILE=dag-default aws sts assume-role $(cat "${config.age.secrets.export-aws-keys.path}")"
-          creds="$(eval "$cmd")"
-
-          access_key_id=$(echo "$creds" | jq -r .Credentials.AccessKeyId)
-          secret_access_key=$(echo "$creds" | jq -r .Credentials.SecretAccessKey)
-          session_token=$(echo "$creds" | jq -r .Credentials.SessionToken)
-
-          echo "
-        export AWS_ACCESS_KEY_ID='$access_key_id'
-        export AWS_SECRET_ACCESS_KEY='$secret_access_key'
-        export AWS_SESSION_TOKEN='$session_token'
-        "
-        }
-
-        main "$@"
-      '';
-    })
-    (writeShellApplication {
-      name = "unset-aws-vars";
-      text = ''
-        main() {
-          echo "
-        unset AWS_ACCESS_KEY_ID
-        unset AWS_SECRET_ACCESS_KEY
-        unset AWS_SESSION_TOKEN
-        "
-        }
-
-        main "$@"
-      '';
-    })
+  home.packages = [
+    export-aws-vars
   ];
 
   programs.rummage = {
@@ -65,10 +34,9 @@ in {
     sessionVariables = {
       AWS_PROFILE = "dag-default";
       PATH = "$PATH:${config.home.homeDirectory}/.dotnet/tools";
-      DOTNET_ROOT = "/opt/homebrew/Cellar/dotnet@8/8.0.15/libexec/";
+      DOTNET_ROOT = "/opt/homebrew/Cellar/dotnet@8/8.0.16/libexec/";
     };
     shellAliases = {
-      uav = "eval $(unset-aws-vars)";
       eav = "eval $(export-aws-vars)";
     };
   };
