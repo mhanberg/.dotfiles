5575be0c868c31c5fbf855f409194ffe5121f111
Mitchell Hanberg <mitch@mitchellhanberg.com>
2026-04-14 13:38:56 -0400

    fix: signing

--- nix/home/work.nix
+++ nix/home/work.nix
@@ -23,4 +23,13 @@ in {
     };
 
   programs.ghostty.package = null;
+
+  programs.ssh.matchBlocks."*".identityAgent = "'~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock'";
+
+  programs.git = {
+    signing.key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDckxDud0PGdGd60v/1SUa0pbWWe46FcVIbuTijwzeZR";
+    settings.gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+    settings.gpg.format = "ssh";
+    settings.commit.gpgSign = true;
+  };
 }
