daac85f73922c906bdce04bb787e6d7bb02e4769
Mitchell Hanberg <mitch@mitchellhanberg.com>
2026-05-19 10:40:37 -0400

    fix: update thing

--- nix/home/work.nix
+++ nix/home/work.nix
@@ -7,7 +7,7 @@ in
   imports = [
     ./themes/rose-pine-moon.nix
     ./services/syncthing.nix
-    (import work.homeManagerModules.default { inherit myLib; })
+    (work.homeManagerModules.default { inherit myLib; })
   ];
 
   home.packages = with pkgs; [ yq-go ];
